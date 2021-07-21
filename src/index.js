235553b4dc3751e3ebe19f58137cadc2101123a2
604karev <604karev@gmail.com>
2021-07-21 18:28:39 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
+const getArrayMax = (array) => Math.max(...array);
+const getArrayMin = (array) => Math.min(...array);
+const getArraySum = (prev, current) => prev + current;
 
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.min = min = (array = []) => array.length > 0 ? getArrayMin(array) : 0;
+exports.max = max = (array = []) => array.length > 0 ? getArrayMax(array) : 0;
+exports.avg = avg = (array = []) => array.length > 0 ? array.reduce(getArraySum) / array.length : 0;
